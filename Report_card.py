a9b114ee0910bbdc6a04c7170e584154c62ddfd5
maznah2013 <maznah201357@gmail.com>
2026-03-16 17:00:08 +0400

    Add files via upload

---
+++ Report_card.py
@@ -0,0 +1,33 @@
+class Student:
+    def __init__(self):
+        self.name=""
+        self.grade=""
+        self.section=""
+    
+class Result(Student):
+    def __init__(self):
+        super().__init__()
+        self.m1=0
+        self.m2=0
+        self.m3=0
+        self.total=0
+        self.avg=0
+
+    def input_details(self):
+        self.name=input("Enter your name: ")
+        self.grade=int(input("Enter your grade: "))
+        self.section=input("Enter your section: ")
+        self.m1=int(input("Enter your marks in Maths: "))
+        self.m2=int(input("Enter your marks in Science: "))
+        self.m3=int(input("Enter your marks in English: "))
+
+    def cal(self):
+        self.total= self.m1 + self.m2 + self.m3
+        self.avg= self.total//3
+
+    def show_details(self):
+        print("The details of the student are: ")
+        print(f"Name: {self.name}")
+        print(f"Grade: {self.grade}")
+        print(f"Section: {self.section}")
+        
---
+++ student.py
@@ -0,0 +1,37 @@
+#PARENT CLASS
+
+class Student:
+    def __init__(self, name, age, grade):
+        self.name=name
+        self.age=age
+        self.grade=grade
+
+    def display(self):
+        print("The details of the student are: ")
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Grade: {self.grade}")
+
+
+#CHILD CLASS
+
+class School(Student):
+    def __init__(self, name, age, grade, house, section):
+        super().__init__(name, age, grade)
+        self.house=house
+        self.section=section
+
+    def show_details(self):
+        print(f"House: {self.house}")
+        print(f"Section: {self.section}")
+
+
+#CREATE AN OBLJECT OF THE CLASS
+
+student1=School("Maznah", 13, 8, "Yellow", "F")
+student1.display()
+student1.show_details()
+
+student2=School("Ayesha", 12, 7, "Blue", "E")
+student2.display()
+student2.show_details()
